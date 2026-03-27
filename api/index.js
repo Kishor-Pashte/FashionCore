6e1522e7ba635243fe3392646f0228a5736d44b2
KISHOR PASHTE <k24145798@gmail.com>
2026-03-27 16:16:24 +0530

    Initial commit (clean, no secrets)

---
+++ api/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
+const productRouter = require("./routes/product");
+const cartRouter = require("./routes/cart");
+const orderRouter = require("./routes/order");
+const checkoutRouter = require("./routes/checkout");
+const {
+  handleMalformedJson,
+  formatCelebrateErrors,
+} = require("./middlewares/handleError");
+
+const app = express();
+
+// mongodb
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.error(err));
+
+// global middlewares
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/auth", authRouter);
+app.use("/users", userRouter);
+app.use("/products", productRouter);
+app.use("/carts", cartRouter);
+app.use("/orders", orderRouter);
+app.use("/checkout", checkoutRouter);
+
+// server status
+app.get("/", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+// format celebrate paramater validation errors
+app.use(formatCelebrateErrors);
+
+app.listen(5000, () => {
+  console.log(`Listening on port ${5000}`);
+});
